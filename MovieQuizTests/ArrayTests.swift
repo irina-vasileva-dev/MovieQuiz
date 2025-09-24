b628a1f9819ade526e4fb71907c5c070a3771f97
Irina Vasileva <alex.grin15@yandex.ru>
2025-09-24 13:40:25 +0300

    Add incommitted changes

--- MovieQuizTests/ArrayTests.swift
+++ MovieQuizTests/ArrayTests.swift
@@ -1,8 +1,29 @@
-//
-//  ArrayTests.swift
-//  MovieQuizTests
-//
-//  Created by Irina Vasileva on 22.09.2025.
-//
 
-import Foundation
+import XCTest
+
+@testable import MovieQuiz
+
+final class ArrayTests: XCTestCase {
+    func testGetValueInRange() throws {
+       // Given
+        let array = [1, 1, 2, 3, 5]
+        
+       // When
+        let value = array[safe: 2]
+        
+       // Then
+        XCTAssertNotNil(value)
+        XCTAssertEqual(value, 2)
+    }
+    
+    func testGetValueOutOfRange() throws {
+        // Given
+        let array = [1, 1, 2, 3, 5]
+        
+       // When
+        let value = array[safe: 20]
+        
+       // Then
+        XCTAssertNil(value)
+    }
+}
